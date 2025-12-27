ebb5255c9c7e27d746c7d5d8a361a985de16a22a
zivbeker42 <zivbeker.39.talpiot@gmail.com>
2025-12-27 11:05:11 +0200

    added get_num_layers in model_config

--- vidur/profiling/common/model_config.py
+++ vidur/profiling/common/model_config.py
@@ -71,6 +71,9 @@ class ModelConfig:
 
     def get_num_kv_heads(self, parallel_config: ParallelConfig):
         return self.num_kv_heads // parallel_config.tensor_parallel_size
+        
+    def get_num_layers(self, parallel_config: ParallelConfig):
+        return self.num_layers // parallel_config.pipeline_parallel_size
 
     def get_head_size(self):
         return self.embedding_dim // self.num_q_heads
